e2a35cd84a3747fe236b6712fd9a7eba7ee083f2
Siriusmart <sirius@siri.ws>
2025-12-25 23:07:23 +0000

    change manifest format

--- src/packages/indexer/component/package_manifest.rs
+++ src/packages/indexer/component/package_manifest.rs
@@ -3,23 +3,34 @@ use std::collections::HashMap;
 use semver::{Version, VersionReq};
 use serde::{Deserialize, Serialize};
 
+/// package.toml manifest
 #[derive(Serialize, Deserialize)]
 #[cfg_attr(feature = "devel", derive(Debug))]
 pub struct PackageManifest {
-    name: String,
-    version: Version,
-    require: HashMap<String, VersionReq>,
+    package: PackageInfo,
+    dependencies: HashMap<String, VersionReq>,
     provides: HashMap<String, PackageComponentType>,
 }
 
 impl PackageManifest {
     pub fn get_name(&self) -> &str {
-        &self.name
+        &self.package.name
     }
 
     pub fn get_version(&self) -> &Version {
-        &self.version
+        &self.package.version
     }
+}
+
+/// has public fields,
+/// but the struct is private
+/// so it is fine.
+/// the struct exists solely to add structure to the toml file
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "devel", derive(Debug))]
+struct PackageInfo {
+    pub name: String,
+    pub version: Version,
 }
 
 #[derive(Serialize, Deserialize)]
