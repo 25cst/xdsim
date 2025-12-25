479eb0a84c2462665c2cadbf9581ed7e38ab6cce
Siriusmart <sirius@siri.ws>
2025-12-25 23:33:42 +0000

    added comments for the indexer module

--- src/packages/indexer/mod.rs
+++ src/packages/indexer/mod.rs
@@ -1,3 +1,31 @@
+//! so far we are only concered with component package,
+//! each component package contains multiple components
+//!
+//! in manifest.toml, provides specifies what components are included in package
+//! in the same directory, it must contain the componentname.[dll/dylib/so]
+//! which is the dynamic library file supported by the operating system
+//!
+//! the indexer creates an index of all packages
+//! the file structure is as follows
+//!
+//! repo-root/
+//! ├── package1/
+//! │   ├── 0.1.0/
+//! │   │   └── package.toml
+//! │   └── 0.1.1/
+//! │       └── package.toml
+//! └── package2/
+//!     ├── 0.1.0/
+//!     │   └── package.toml
+//!     └── 0.1.1/
+//!         └── package.toml
+//! repo2-root/
+//! └── package3/
+//!     └── 0.1.0/
+//!         └── package.toml
+//!
+//! package.toml specs is found in component/package_manifest
+
 mod component;
 mod error;
 pub use error::Error;
