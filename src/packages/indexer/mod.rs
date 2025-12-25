3a161876c4904c3ae53ded4b514686ab67fbaba6
Siriusmart <sirius@siri.ws>
2025-12-25 23:33:53 +0000

    updated comments

--- src/packages/indexer/mod.rs
+++ src/packages/indexer/mod.rs
@@ -1,12 +1,12 @@
+//! the indexer creates an index of all packages
+//! the file structure is as follows
+//!
 //! so far we are only concered with component package,
 //! each component package contains multiple components
 //!
 //! in manifest.toml, provides specifies what components are included in package
 //! in the same directory, it must contain the componentname.[dll/dylib/so]
 //! which is the dynamic library file supported by the operating system
-//!
-//! the indexer creates an index of all packages
-//! the file structure is as follows
 //!
 //! repo-root/
 //! ├── package1/
